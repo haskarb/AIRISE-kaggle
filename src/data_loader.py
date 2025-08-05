27fbca360fbf942289b569df48564b18cd92eec2
Bhaskar Dhariyal <bdhariyal94@gmail.com>
2025-08-05 22:22:05 +0100

    [WIP] Add data integrity

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,29 +1,24 @@
 import polars as pl
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple
 from pathlib import Path
 from dataclasses import dataclass
 
 @dataclass
 class DataLoader:
     file_path: Path 
-
-    def load_data(self, split="train") -> pl.DataFrame:
-
-        if split == "train":
-            file_path = self.file_path / "train.csv"
-        elif split == "test":
-            file_path = self.file_path / "test.csv"
-        else:
-            raise ValueError("Invalid split value. Use 'train' or 'test'.")
-
-        return pl.read_csv(file_path)
-
-    def list_files(self) -> List[str]:
-        return [str(file) for file in self.file_path.glob("*.csv")]
     
 
+    def get_train_data(self) -> pl.DataFrame:
+        return pl.read_csv(self.file_path / "train.csv")
+
+    def get_test_data(self) -> pl.DataFrame:
+        return pl.read_csv(self.file_path / "test.csv")
+
+    def load_data(self) -> Tuple[pl.DataFrame, pl.DataFrame]:
+        return self.get_train_data(), self.get_test_data()
 
 if __name__ == "__main__":
     data_loader = DataLoader(file_path=Path("data/raw"))
-    df = data_loader.load_data("train")
-    print(df.head())
+    df_train, df_test = data_loader.load_data()
+    print(df_train.head())
+    print(df_test.head())
